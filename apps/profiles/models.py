20c2f941846c624e965031383a46f2e9173b5386
Aman Ahuja <amanahuja@gmail.com>
2015-11-13 15:54:35 -0800

    files from last year

---
+++ apps/profiles/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from idios.models import ProfileBase
+
+from workout.models import Team
+
+class Profile(ProfileBase):
+  """
+  PROFILE
+  -------
+  Fields available for profile and bio fields. 
+
+  """
+  
+  #Name that shows up on your profile
+  name = models.CharField(_("name"), 
+    max_length=50, null=True, blank=True)
+
+  #About / Description / Bio
+  about = models.TextField(_("about"), 
+    null=True, blank=True)
+
+  #Graduating Year
+  FRESHMAN = 'FR'; SOPHOMORE = 'SO'; 
+  JUNIOR = 'JR'; SENIOR = 'SR'; GRADUATE = 'GR'
+  COACH = 'CO'
+
+  GRADUATING_CLASS_CHOICES = (
+    (FRESHMAN, 'Freshman'),
+    (SOPHOMORE, 'Sophomore'),
+    (JUNIOR, 'Junior'),
+    (SENIOR, 'Senior'),
+    (GRADUATE, 'Graduate Student'),
+    (COACH, 'Coach')
+    )
+  
+  graduating_class = models.CharField(max_length=2,
+    choices=GRADUATING_CLASS_CHOICES,
+    default=SOPHOMORE)
+
+  #Queens or Tarts?
+  TART = 'TR'; PIE = 'PI'
+  DESSERT_CHOICES = (
+    (TART, 'Tart'),
+    (PIE, 'Pie')
+    )
+  dessert = models.CharField(max_length=2, 
+    choices=DESSERT_CHOICES, 
+    default=PIE)
+ 
+  #Teams that the user is part of. 
+  teams = models.ManyToManyField(Team, blank=True)
+
+  #URL (href) to a photo to use in Bio
+  photo_link = models.URLField(_("photo_link"), null=True, 
+    blank=True, verify_exists=False)
+
+  #location (DISABLED)
+  #location  = models.CharField(_("location"), max_length=40, null=True, blank=True)
+
+  # ----- Methods -------
+  def is_upperclass(self):
+    """Checks if upperclassman"""
+    return self.graduating_class in (self.JUNIOR, self.SENIOR, self.GRADUATE)
+
+  def class_str(self):
+    """Returns string rep for graduating class"""
+    CLASS_STR_DICT = {
+        'FR': 'Freshman',  
+        'SO': 'Sophomore', 
+        'JR': 'Junior', 
+        'SR': 'Senior',
+        'GR': 'Graduate Student', 
+        'CO': 'Coach',
+        }
+    return CLASS_STR_DICT[self.graduating_class]
+
+  def dessert_str(self):
+    """Returns string rep for graduating class"""
+    DESSERT_STR_DICT = {
+        'PI': 'Pie', 
+        'TR': 'Tart',
+        }
+    return DESSERT_STR_DICT[self.dessert]
---
+++ urls.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+from pinax.apps.account.openid_consumer import PinaxConsumer
+
+
+handler500 = "pinax.views.server_error"
+
+urlpatterns = patterns("",
+    url(r"^$", direct_to_template, {
+        "template": "homepage.html",
+    }, name="home"),
+    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^about/", include("about.urls")),
+    url(r"^account/", include("pinax.apps.account.urls")),
+    url(r"^openid/", include(PinaxConsumer().urls)),
+    
+    #Override profile view before idios include
+    url(r"^profiles/test$", 
+       'workout.views.profiles', 
+       name = "new_profiles_view"),
+
+    url(r"^profiles/", include("idios.urls")),
+    url(r"^notices/", include("notification.urls")),
+    url(r"^announcements/", include("announcements.urls")),
+
+    url(r"^workouts/", include("workout.urls")),
+    url(r'scoreboard$', 'workout.views.scoreboard', name='scoreboard'), 
+    #url(r"^enter_workout$", "workout.views.index", name="workout_home")
+  )
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns("",
+        url(r"", include("staticfiles.urls")),
+    )
