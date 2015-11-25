8bcf2d7e02858a3f8cd68db84692059b031cbd01
Aman Ahuja <amanahuja@gmail.com>
2015-11-24 16:30:04 -0800

    updated settings to match common configuration for local env

--- settings_example.py
+++ settings_example.py
@@ -29,7 +29,7 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "/sites/piequeens/workout_wars/db/dev.db",                       # Or path to database file if using sqlite3.
+        "NAME": "db/dev.db",                       # Or path to database file if using sqlite3.
         "USER": "",                             # Not used with sqlite3.
         "PASSWORD": "",                         # Not used with sqlite3.
         "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
@@ -113,7 +113,7 @@ MIDDLEWARE_CLASSES = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = "workout_wars.urls"
+ROOT_URLCONF = "workout-wars.urls"
 
 TEMPLATE_DIRS = [
     os.path.join(PROJECT_ROOT, "templates"),
