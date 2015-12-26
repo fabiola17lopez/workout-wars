053bba7a5b4f678f64bfc2b245816f599611a847
Aman Ahuja <amanahuja@gmail.com>
2015-12-26 13:57:14 -0800

    pep 8 refactor

--- workout/views.py
+++ workout/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse, HttpResponseRedirect
+# from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-#from django.core.urlresolvers import reverse_lazy
+# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.core.urlresolvers import reverse_lazy
 
 from collections import defaultdict
 from decimal import Decimal
@@ -14,135 +14,144 @@ from django.utils import simplejson
 from workout.models import Workout, Activity, Team, User
 from apps.profiles.models import Profile
 
+import operator
+
+
 @login_required
 def index(request):
-  """
-  Index View
-  -----------
-  Shows a list of recent workouts by all Users
-  Perhaps a list of current user's recent workouts
-  Good place for a scoreboard.
-  """
-  try:
-    recent_workouts = Workout.objects.all().order_by('workout_date')
-  except ObjectDoesNotExist:
-    recent_workouts = []
+    """
+    Index View
+    -----------
+    Shows a list of recent workouts by all Users
+    Perhaps a list of current user's recent workouts
+    Good place for a scoreboard.
+    """
+    try:
+        recent_workouts = Workout.objects.all().order_by('workout_date')
+    except ObjectDoesNotExist:
+        recent_workouts = []
 
-  return render_to_response('workout/workouts_home.html',
-      {'workouts': recent_workouts},
-      context_instance=RequestContext(request)
-      )
+    return render_to_response(
+        'workout/workouts_home.html',
+        {'workouts': recent_workouts},
+        context_instance=RequestContext(request)
+    )
 
-import operator
+
 def indiv(request):
-  pdict = defaultdict(int)
-  for ww in Workout.objects.all():
-    pdict[ww.user.username] += float(ww.score)
-  pdict = dict(pdict)
+    pdict = defaultdict(int)
+    for ww in Workout.objects.all():
+        pdict[ww.user.username] += float(ww.score)
 
+    pdict = dict(pdict)
 
-  rankedlist = sorted(pdict.iteritems(),
-      key=operator.itemgetter(1),
-      reverse=True)
+    rankedlist = sorted(
+        pdict.iteritems(),
+        key=operator.itemgetter(1),
+        reverse=True)
 
-  userinfo = []
-  for user,score in rankedlist:
-    pp = Profile.objects.filter(user__username__iexact=user)[0]
-    userinfo.append((user, score, pp.class_str(), pp.dessert_str()))
+    userinfo = []
+    for user, score in rankedlist:
+        pp = Profile.objects.filter(user__username__iexact=user)[0]
+        userinfo.append((user, score, pp.class_str(), pp.dessert_str()))
 
-  return render_to_response('workout/workouts_indiv.html',
-      {'ranked_players': userinfo},
-      context_instance=RequestContext(request)
-      )
+    return render_to_response(
+        'workout/workouts_indiv.html',
+        {'ranked_players': userinfo},
+        context_instance=RequestContext(request)
+    )
 
 
 def test(request):
-  pdict = defaultdict(int)
-  for ww in Workout.objects.all():
-    pdict[ww.user.username] += float(ww.score)
+    pdict = defaultdict(int)
+    for ww in Workout.objects.all():
+        pdict[ww.user.username] += float(ww.score)
 
-  rankedlist = sorted(pdict.iteritems(),
-      key=operator.itemgetter(1),
-      reverse=True)
+    rankedlist = sorted(
+        pdict.iteritems(),
+        key=operator.itemgetter(1),
+        reverse=True)
 
-  return render_to_response('workout/workouts_indiv.html',
-      {'ranked_players': rankedlist},
-      context_instance=RequestContext(request)
-      )
+    return render_to_response(
+        'workout/workouts_indiv.html',
+        {'ranked_players': rankedlist},
+        context_instance=RequestContext(request)
+        )
+
 
 def profiles(request):
-  pass
+    pass
+
 
 def scoreboard(request):
-  """
-  Scoreboard View
-  ---------------
-  Shows the scoreboards.
-  1. Total for each class
-  2. Total for each team
-  """
+    """
+    Scoreboard View
+    ---------------
+    Shows the scoreboards.
+    1. Total for each class
+    2. Total for each team
+    """
 
-  # Commenting out b/c not using pie/tart distinction in 2015
-  # class_list_PI, class_dict_PI = _gclass_query(dessert = 'PI')
-  # class_list_TR, class_dict_TR = _gclass_query(dessert = 'TR')
-  # ... instead:
-  class_list, class_dict = _gclass_query()
+    # Commenting out b/c not using pie/tart distinction in 2015
+    # class_list_PI, class_dict_PI = _gclass_query(dessert = 'PI')
+    # class_list_TR, class_dict_TR = _gclass_query(dessert = 'TR')
+    # ... instead:
+    class_list, class_dict = _gclass_query()
 
+    # For Teams
+    all_teams = [t.name for t in Team.objects.all()]
+    team_scores = []
 
-  ## For Teams
-  all_teams = [t.name for t in Team.objects.all()]
-  team_scores = []
-
-  for team in all_teams:
-    q = Workout.objects.filter(
-        user__profile__teams__name = team
-        )
-    #Get total workout score for each Team.
+    for team in all_teams:
+        q = Workout.objects.filter(
+            user__profile__teams__name=team
+            )
+    # Get total workout score for each Team.
     iscore = 0
     for iworkout in q:
-      iscore += iworkout.score
+        iscore += iworkout.score
 
     team_scores.append([team, iscore])
 
-  return render_to_response(
-      'workout/scoreboard.html',
-      {
-          'team_scores': team_scores,
-          # 'class_dict_PI': dict(class_dict_PI),
-          # 'class_list_PI': simplejson.dumps(class_list_PI),
-          # 'class_dict_TR': dict(class_dict_TR),
-          # 'class_list_TR': simplejson.dumps(class_list_TR),
-          'class_dict_BOTH': dict(class_dict),
-          'class_list_BOTH': simplejson.dumps(class_list),
-      },
-      context_instance=RequestContext(request)
-      )
+    return render_to_response(
+        'workout/scoreboard.html',
+        {
+            'team_scores': team_scores,
+            # 'class_dict_PI': dict(class_dict_PI),
+            # 'class_list_PI': simplejson.dumps(class_list_PI),
+            # 'class_dict_TR': dict(class_dict_TR),
+            # 'class_list_TR': simplejson.dumps(class_list_TR),
+            'class_dict_BOTH': dict(class_dict),
+            'class_list_BOTH': simplejson.dumps(class_list),
+        },
+        context_instance=RequestContext(request)
+    )
 
 
 def _gclass_query(dessert=None):
-  """
-  Calculates workout scores by class
-  with option to subdivid by dessert (pies vs tarts)
-  """
-  CLASS_YEARS = ['FR', 'SO', 'JR', 'SR', 'GR', 'CO']
-  class_dict = defaultdict(Decimal)
+    """
+    Calculates workout scores by class
+    with option to subdivid by dessert (pies vs tarts)
+    """
+    CLASS_YEARS = ['FR', 'SO', 'JR', 'SR', 'GR', 'CO']
+    class_dict = defaultdict(Decimal)
 
-  class_list = []
-  class_list.append( ['Class', 'Points'] )
+    class_list = []
+    class_list.append(['Class', 'Points'])
 
-  for gclass in CLASS_YEARS:
-    # Get number of players in class
-    if dessert is not None:
-        n_players = len(
-            User.objects.filter(
-                profile__dessert__iexact=dessert).filter(
-                profile__graduating_class__iexact=gclass)
-            )
-    else:  # i.e., if dessert is None
-        n_players = len(
-            User.objects.filter(
-                profile__graduating_class__iexact=gclass)
-            )
+    for gclass in CLASS_YEARS:
+        # Get number of players in class
+        if dessert is not None:
+            n_players = len(
+                User.objects.filter(
+                    profile__dessert__iexact=dessert).filter(
+                    profile__graduating_class__iexact=gclass)
+                )
+        else:  # i.e., if dessert is None
+            n_players = len(
+                User.objects.filter(
+                    profile__graduating_class__iexact=gclass)
+                )
 
     iscore = 0
 
@@ -157,127 +166,133 @@ def _gclass_query(dessert=None):
             user__profile__graduating_class__iexact=gclass
         )
 
-    #get total workout score for each Year
+    # get total workout score for each Year
     for iworkout in q:
-      iscore += iworkout.score
+        iscore += iworkout.score
 
-    #Normalize scores by n_players in the class
+    # Normalize scores by n_players in the class
     # with: iscore / n_players
     if n_players > 0:
-      normed = float(iscore / n_players)
+        normed = float(iscore / n_players)
     elif n_players == 0:
-      normed = 0
-    class_list.append ([gclass, normed])
-    #Tuple with both total and normalized scores
+        normed = 0
+    class_list.append([gclass, normed])
+    # Tuple with both total and normalized scores
     class_dict[gclass] = (normed, iscore)
 
-  return class_list, class_dict
+    return class_list, class_dict
+
 
 @login_required
 def add(request):
-  """
-  Add View
-  ------------
-  Fill out a CreateForm for a workout
-  Simple form entry here, maybe a default view?
-  Need dropdown for workout options
-  """
-  context_data = {}
+    """
+    Add View
+    ------------
+    Fill out a CreateForm for a workout
+    Simple form entry here, maybe a default view?
+    Need dropdown for workout options
+    """
+    context_data = {}
 
-  if request.method == 'POST':
+    if request.method == 'POST':
+        '''
+        Form has been submitted. Run checks and cleans and save
+        '''
+        # Get all data here
+        workout = Workout()
+
+        workout.duration = request.POST['duration']
+        if 'with_other_class' in request.POST:
+            if request.POST['with_other_class'] == 'on':
+                workout.with_other_class = True
+
+        # Manage Dates
+        try:
+            workout.workout_date = request.POST['workout_date']
+        except:
+            year = request.POST['workout_date_year']
+            month = request.POST['workout_date_month']
+            day = request.POST['workout_date_day']
+            workout.workout_date = '{}-{}-{}'.format(year, month, day)
+
+        workout.activity_id = request.POST['activity']
+
+        # Populate user data
+        user = request.user
+        workout.user_id = user.pk
+
+        # Create bound form
+        # form = WorkoutFormCreate(data = workout_data )
+
+        try:
+            # VALID DATA
+            workout.save()
+            context_data['message'] = "Data submitted."
+
+            return render_to_response(
+                'workout/workouts_submit.html',
+                context_data, context_instance=RequestContext(request),
+                )
+        except:
+            # INVALID DATA
+            context_data['message'] = \
+                "Invalid Data Submitted. Please try again."
+
     '''
-    Form has been submitted. Run checks and cleans and save
+    ELSE if request.method != POST)
+    Form has not yet been submitted. Create new form.
     '''
-    #Get all data here
-    workout = Workout()
 
-    workout.duration = request.POST['duration']
-    if 'with_other_class' in request.POST:
-      if request.POST['with_other_class'] == 'on':
-        workout.with_other_class = True
+    # Create unbound form
+    context_data['form'] = WorkoutFormCreate(user=request.user)
+    # form = WorkoutFormCreate(user = request.user )
 
-    #Manage Dates
-    try:
-      workout.workout_date = request.POST['workout_date']
-    except:
-      year = request.POST['workout_date_year']
-      month = request.POST['workout_date_month']
-      day = request.POST['workout_date_day']
-      workout.workout_date = '{}-{}-{}'.format(year, month, day)
-
-    workout.activity_id = request.POST['activity']
-
-    #Populate user data
-    user = request.user
-    workout.user_id = user.pk
-
-    #Create bound form
-    #form = WorkoutFormCreate(data = workout_data )
-
-    try:
-      #VALID DATA
-      workout.save()
-      context_data['message']= "Data submitted."
-
-      return render_to_response('workout/workouts_submit.html',
-          context_data, context_instance=RequestContext(request),
-          )
-    except:
-      #INVALID DATA
-      context_data['message']= "Invalid Data Submitted. Please try again."
-
-  '''
-  ELSE if request.method != POST)
-  Form has not yet been submitted. Create new form.
-  '''
-
-  #Create unbound form
-  context_data['form'] = WorkoutFormCreate(user = request.user )
-  #form = WorkoutFormCreate(user = request.user )
-
-  return render_to_response('workout/workout_form.html',
-      context_data,
-      context_instance=RequestContext(request),
-      )
+    return render_to_response(
+        'workout/workout_form.html',
+        context_data,
+        context_instance=RequestContext(request),
+        )
 
 from django.forms import ModelForm, ModelChoiceField
 from django.forms.fields import DecimalField, BooleanField, DateField
-from django.contrib.admin.widgets import AdminDateWidget
+# from django.contrib.admin.widgets import AdminDateWidget
 from datetime import datetime
 from django.forms.extras.widgets import SelectDateWidget
 
+
 class WorkoutFormCreate(ModelForm):
-  duration = DecimalField(
-      min_value=0, max_value = 40, max_digits=5,
-      label = "Duration (15 min blocks)",
-      help_text = "e.g., for a 30 minute workout, enter '2'."
-      )
+    duration = DecimalField(
+        min_value=0, max_value=40, max_digits=5,
+        label="Duration (15 min blocks)",
+        help_text="e.g., for a 30 minute workout, enter '2'."
+        )
 
-  with_other_class = BooleanField(
-      label = "With different class?",
-      help_text = "BONUS POINTS! Select this if you worked out with a teammates from a different graduating class, like a Sophomore with a Junior."
-      )
+    with_other_class = BooleanField(
+        label="With different class?",
+        help_text="BONUS POINTS! Select this if you worked out with" +
+        "a teammates from a different graduating class, like a" +
+        "Sophomore with a Junior."
+        )
 
-  workout_date = DateField(
-      initial = datetime.today,
-      widget = SelectDateWidget(years=(2014,2015)),
-      label = "Workout Date",
-      help_text = "When did you do the workout?"
-      )
+    workout_date = DateField(
+        initial=datetime.today,
+        widget=SelectDateWidget(years=(2014, 2015)),
+        label="Workout Date",
+        help_text="When did you do the workout?"
+        )
 
-  activity = ModelChoiceField(
-      queryset = Activity.objects.order_by('description'))
+    activity = ModelChoiceField(
+        queryset=Activity.objects.order_by('description'))
 
-  class Meta:
-    model = Workout
-    fields = [   #Appears in the form in this order:
-      'workout_date',
-      'activity',
-      'with_other_class',
-      'duration', ]
-    exclude = ['user',]
+    class Meta:
+        model = Workout
+        fields = [   # Appears in the form in this order:
+            'workout_date',
+            'activity',
+            'with_other_class',
+            'duration', ]
+        exclude = ['user', ]
 
-  def __init__(self, user=None, *args, **kwargs):
-    super(WorkoutFormCreate, self).__init__(*args, **kwargs)
-    self._user = user
-
+    def __init__(self, user=None, *args, **kwargs):
+        super(WorkoutFormCreate, self).__init__(*args, **kwargs)
+        self._user = user
