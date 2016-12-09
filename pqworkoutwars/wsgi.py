cd576a9e4d71882607a34dd7b0f47d47e1bf6f49
Fabiola Lopez <fabiola17lopez@berkeley.edu>
2016-12-08 17:51:28 -0800

    updated wsgi.py location

---
+++ pqworkoutwars/wsgi.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+from django.core.handlers.wsgi import WSGIHandler
+
+import pinax.env
+
+#---------
+import os
+import sys
+
+sys.path.append('/sites/piequeens/')
+sys.path.append('/sites/piequeens/ENV')
+#---------
+
+# setup the environment for Django and Pinax
+pinax.env.setup_environ(__file__)
+
+# set application for WSGI processing
+application = WSGIHandler()
